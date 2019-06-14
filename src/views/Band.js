b2fcc13c5470a0530845fdc68ae0f80368758b26
Marcel Kerkveld <mke21@live.nl>
2019-06-14 17:28:40 +0200

    freeze

--- src/views/Band.js
+++ src/views/Band.js
@@ -5,7 +5,14 @@ module.exports = {
     view: function(){
         return [
             m("h1", "Over ons"),
-            m("p", "RedBeat is een rockband met als basis Fijnaart. Wij spelen gitaar rock, van classic rock tot modern."),
+            m("p", "RedBeat is een rockband met als basis Fijnaart. Wij spelen gitaar rock, van classic rock tot modern"),
+            m("p", "U kunt kontakt met ons opnemen via facebook"),
+            m("ul.linklist", [
+                m("li", m("a", {href: "https://www.facebook.com/RedBeatBand" ,target: "blank"},
+                          m("img", {src: "/images/facebook.png"}))),
+                m("li", m("a", {href: "https://www.youtube.com/channel/UCY2VOtxSpLbKI0zZuDi8KJg" ,target: "blank"},
+                          m("img", {src: "/images/youtube.png"})))
+            ]),
             m("iframe", {
                 width: "560",
                 height: "315",
@@ -14,7 +21,7 @@ module.exports = {
                 allow: "accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture",
                 allowfullscreen: true
             })
-
+            
         ]
     }
 }
