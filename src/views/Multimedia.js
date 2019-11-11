c4639ee491d06b588999ff6ed42875b278267bc9
Marcel Kerkveld <mke21@live.nl>
2019-11-11 20:38:23 +0100

    added audio

--- src/views/Multimedia.js
+++ src/views/Multimedia.js
@@ -1,11 +1,13 @@
 // src/views/Multimedia.js
 var m = require("mithril").default;
-var Fotos = require("./Fotos.js");
+var Fotos = require("./Fotos");
+var Music = require("./Mp3");
 
 module.exports = {
     view: function(){
         return [
 					m(Fotos),
+					m(Music)
         ]
     }
 }
