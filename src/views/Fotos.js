995b745c6b297ef043c4d3ede43aeabd8d2fe946
Marcel Kerkveld <4930603+mke21@users.noreply.github.com>
2019-11-15 22:18:34 +0100

    added arrows

--- src/views/Fotos.js
+++ src/views/Fotos.js
@@ -3,18 +3,6 @@ import m from "mithril";
 var Images = require("../models/Images").default;
 var src = "";
 
-/*export default {
-	oninit: Images.loadList,
-    view: function(){
-        return [
-          m("h1", "Fotos"),
-          m("img", {src: "/slider/" + Images.current, style: "max-width:100%"} ),
-					m("p"),
-					m("button", {onclick: function() {src = Images.next()} }, "next"),
-					m("button", {onclick: function() {src = Images.previous()}}, "previous")
-        ]
-    }
-}*/
 export default {
 	oninit: Images.loadList,
     view: function(){
@@ -23,23 +11,13 @@ export default {
 						m("div.mySlides", {style: "with:100%;"}, [
 							m("img", {src: "/slider/" + Images.current, style: "max-width:100%"} ),
 						]),
-						m("a", {class: "prev", onclick: function() {src = Images.previous()}}, "<"),
-						m("a", {class: "next", onclick: function() {src = Images.next()} }, ">")
+						m("a", {class: "prev", onclick: function() {src = Images.previous()}}, 
+						m("img", {src: "/images/prev.png"})),
+						m("a", {class: "next", onclick: function() {src = Images.next()}}, 
+						m("img", {src: "/images/next.png"})),
+
 					])
         ]
     }
 }
 
-
-/*export default {
-	oninit: Images.LoadList,
-	view: function() {
-		return [
-			m('div.container', [
-				m("img", {src: "/slider/" + Images.current, style: "max-width:100%"} ),
-				m("a", {class: 'slgprev w3-text-white', onclick: function() {src = Images.previous()}}, "<")
-			])
-		]
-	}
-}
-*/
