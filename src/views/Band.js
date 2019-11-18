ef20402c7a93beab6d72c08e79a198d1d68c48e7
Marcel Kerkveld <4930603+mke21@users.noreply.github.com>
2019-11-18 21:30:11 +0100

    fixed facebook icon height

--- src/views/Band.js
+++ src/views/Band.js
@@ -4,12 +4,14 @@
 	export default {
 			view: function(){
 					return [
+						//m("img", {src: "/images/logo-sm.png", id: "logo"}),
 							m("h1", "Over ons"),
 							m("p", "Rode bieten met stevige bite, wij zijn RedBeat! Een rock coverband uit West-Brabant. Wij spelen gitaar rock, van classic tot modern, van mainstream tot alternative. Met twee gitaren, bas, drum en zang: een garantie voor een avond volop genieten!"),
 							m("p", [
 								"U kunt contact met ons opnemen via facebook.",
-								m("li", {href: "https://www.facebook.com/RedBeatBand" ,target: "blank"},
-									m("img", {src: "/images/facebook.png"}))
+								m("br"),
+								m("a", {href: "https://www.facebook.com/RedBeatBand" ,target: "blank"},
+									m("img", {src: "/images/facebook.png", style: "height: 50px; width: 50px;"}))
 							] ),
             /*m("iframe", {
                 width: "560",
