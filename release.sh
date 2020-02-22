9be703543a1cfb7e082b8a92e73f135b715d4cc4
Marcel Kerkveld <mke21@live.nl>
2020-02-22 12:42:49 +0100

    https

--- release.sh
+++ release.sh
@@ -1,6 +1,6 @@
-rclone copy --progress bin band:/domains/redbeatband.nl/public_html/bin
-rclone copy --progress images band:/domains/redbeatband.nl/public_html/images
-rclone copy --progress index.html band:/domains/redbeatband.nl/public_html/
-rclone copy --progress data band:/domains/redbeatband.nl/public_html/data
-rclone copy --progress music band:/domains/redbeatband.nl/public_html/music
-rclone copy --progress slider band:/domains/redbeatband.nl/public_html/slider
+rclone copy --progress bin band:/domains/redbeatband.nl/private_html/bin
+rclone copy --progress images band:/domains/redbeatband.nl/private_html/images
+rclone copy --progress index.html band:/domains/redbeatband.nl/private_html/
+rclone copy --progress data band:/domains/redbeatband.nl/private_html/data
+rclone copy --progress music band:/domains/redbeatband.nl/private_html/music
+rclone copy --progress slider band:/domains/redbeatband.nl/private_html/slider
